dc3aacf7ebee2f4ee5c84cfb223b5efa2179bf09
Egor Bazakin <egor.bazakin.09@gmail.com>
2025-04-16 19:55:51 +0300

    Products localization

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,32 +1,34 @@
 import ButtonWithIcon from "@components/Buttons/ButtonWithIcon";
 import { FaStar } from "react-icons/fa";
 import { ProductType } from "@app-types/types";
+import { useTranslation } from "react-i18next";
 
 const ProductCard = ({ image, name, price, discountedPrice, rating }: ProductType) => {
+  const { t } = useTranslation();
   return (
-    <div className="w-[350px] h-[407px] bg-white rounded-xl shadow-md flex flex-col justify-between p-4">
+    <div className="w-[350px] h-[407px] bg-white rounded-xl shadow-md flex flex-col justify-between p-[1em]">
       <img src={image} alt={name} className="w-full h-[250px] object-contain" />
-      <div className="flex justify-between items-start mt-2 min-h-[60px]">
+      <div className="flex justify-between items-start mt-[0.5em] min-h-[60px]">
         <p className="text-base font-semibold text-black max-w-[65%]">{name}</p>
         <div className="text-right text-morange">
           {discountedPrice ? (
             <>
-              <p className="text-base font-bold">{discountedPrice}</p>
-              <p className="line-through text-sm text-morange">{price}</p>
+              <p className="text-base font-bold">{discountedPrice} {t("currency")}</p>
+              <p className="line-through text-sm text-morange">{price} {t("currency")}</p>
             </>
           ) : (
-            <p className="text-base font-bold">{price}</p>
+            <p className="text-base font-bold">{price} {t("currency")}</p>
           )}
         </div>
       </div>
-      <div className="flex justify-between items-center mt-4">
-        <div className="flex items-center gap-1 text-sm text-gray-600">
+      <div className="flex justify-between items-center mt-[1em]">
+        <div className="flex items-center gap-[0.25em] text-sm text-gray-600">
           <FaStar className="text-yellow-500" />
           {rating.toFixed(1)}
         </div>
         <ButtonWithIcon
-          text="Купить"
-					className="bg-morange text-white px-4 py-2 rounded-lg text-sm font-semibold"
+          text={t("products.buy")}
+					className="bg-morange text-white w-[10em] py-[0.5em] rounded-lg text-sm font-semibold"
 				/>
       </div>
     </div>
--- src/language/i18n.ts
+++ src/language/i18n.ts
@@ -8,6 +8,7 @@ i18n.use(initReactI18next).init({
         en: {
 					translation: {
             helloWorld: "Hello World!",
+						currency: "₽",
 						
 						footer: {
 							cart: "Cart",
@@ -15,11 +16,17 @@ i18n.use(initReactI18next).init({
 							contacts: "Contacts",
 							terms: "Terms of Service",
 						},
+						products: {
+							buy: "Buy",
+							wirelessHeadphones: "Wireless Headphones",
+							headphones: "Headphones",
+						},
 					}
         },
         ru: {
 					translation: {
             helloWorld: "Привет Мир!",
+						currency: "$",
 
 						footer: {
 							cart: "Корзина",
@@ -27,17 +34,28 @@ i18n.use(initReactI18next).init({
 							contacts: "Контакты",
 							terms: "Условия сервиса",
 						},
+						products: {
+							buy: "Купить",
+							wirelessHeadphones: "Беспроводные наушники",
+							headphones: "Наушники",
+						},
 					}
         },
         kz: {
 					translation: {
             helloWorld: "Сәлем Әлем!",
+						currency: "₸",
 
 						footer: {
 							cart: "Себет",
 							favourite: "Таңдаулылар",
 							contacts: "Байланыс",
 							terms: "Қызмет шарттары",
+						},
+						products: {
+							buy: "Сатып алу",
+							wirelessHeadphones: "Сымсыз құлаққаптар",
+							headphones: "Құлаққаптар",
 						},
 					}
         }
