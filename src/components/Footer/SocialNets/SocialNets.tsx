7bc08bf1516b2f1e9ad6934c9fdc915f9d9fd6b4
Egor Bazakin <egor.bazakin.09@gmail.com>
2025-04-16 17:39:22 +0300

    SocialNets improve: adaptive size

--- src/components/Footer/SocialNets/SocialNets.tsx
+++ src/components/Footer/SocialNets/SocialNets.tsx
@@ -10,13 +10,13 @@ interface SocialNextsProps {
 
 const SocialNets = ({ vkURL, tgURL, whURL }: SocialNextsProps) => {
  const socialLinks = [
-  { href: vkURL, icon: <SlSocialVkontakte className="w-6 h-6" /> },
-  { href: tgURL, icon: <RiTelegramLine className="w-6 h-6" /> },
-  { href: whURL, icon: <FaWhatsapp className="w-6 h-6" /> },
+  { href: vkURL, icon: <SlSocialVkontakte className="w-[1.5em] h-[1.5em]" /> },
+  { href: tgURL, icon: <RiTelegramLine className="w-[1.5em] h-[1.5em]" /> },
+  { href: whURL, icon: <FaWhatsapp className="w-[1.5em] h-[1.5em]" /> },
 ];
 
 return (
-  <div className="flex flex-row items-center bg-white gap-3">
+  <div className="flex flex-row items-center justify-center bg-white gap-2 w-full max-w-full">
     {socialLinks.map(({ href, icon }, index) => (
       <a key={index} href={href} target="_blank" rel="noopener noreferrer">
         {icon}
